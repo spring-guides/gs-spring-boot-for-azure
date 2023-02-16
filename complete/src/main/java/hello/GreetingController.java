633eafb9c9f400c7036d154661a5599d3361314c
Rujun Chen <rujche@microsoft.com>
2023-02-16 14:15:54 +0800

    Fix errors in README.adoc.

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class GreetingController {
 
-	@RequestMapping("/greeting")
+	@RequestMapping("/")
 	public @ResponseBody String greeting() {
 		return "Hello World";
 	}
